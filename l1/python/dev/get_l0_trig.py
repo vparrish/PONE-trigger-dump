0ce0b0dcfea8090647f63a71a4e9174c9a9e4e88
jmgarriz <garrizje@msu.edu>
2024-05-21 16:28:31 -0400

    Redid pulsequeue class, struggling on other things now

--- l1/python/dev/get_l0_trig.py
+++ l1/python/dev/get_l0_trig.py
@@ -48,7 +48,26 @@ class ModuleTrigger:
 
 
 #def create_pulses():
+class PulseQueue:
+    def __init__(self, pulses):
+        self.pulses = pulses
+        self.index = 0
     
+    def empty(self):
+        return self.index >= len(self.pulses)
+    
+    def next(self):
+        if self.empty():
+            raise StopIteration("No more pulses")
+        return self.pulses[self.index]
+    
+    def advance(self):
+        while self.index < len(self.pulses):
+            self.index += 1
+            if self.index < len(self.pulses) and self.pulses[self.index].get_charge() >= 0.25:
+                break        
+
+"""    
 class PulseQueue:
     def __init__(self, pulses):
         self.it = iter(pulses)
@@ -60,14 +79,16 @@ class PulseQueue:
 
     def next_pulse(self):
         return next(self.it)
-
+    
+    #this function is buggy and does not work properly
     def advance(self):
         while self.it != self.end:
-            self.it += 1 
-            pulse = next(self.it)
-            if self.it != self.end and pulse.charge >= 0.25:
+            #self.it += 1
+            pulse =self.pulses[i]
+            print(pulse)
+            if i != self.end and pulse.charge >= 0.25:
                 break
-
+"""
 
 #make! some! functions! 
 
@@ -76,48 +97,39 @@ def findModuleMultiplicity(modules , timeWindow):
     for mk, pmts in modules.items():
         maxMult = 0
         #print(value[:][0])
-        #print(pmt)
-        #I think I'm misunderstanding what the modules map contains in it?? 
-        ##this is pulsequeue??? go look at assessmuons to verify 
+        #print(pmt) 
         for pmt, pulses, in pmts:
-            #print(pmt)
+            #print(pmts)
             #print(pulses.pulses[0].charge)
             if(pulses.pulses[0].charge <0.25):
+                print("boo")
                 pulses.advance()
                 #print(pulses[i]) #this charge cutoff stands in for the firmware trigger
                 if(len(pulses) == 0):
-                    #idk if erase will work
                     print("i am erasing")
                     pmts.remove(pmt)
         #this needs to be a specific structure 
         trigger = ModuleTrigger(mk, 0 ,0)
         
-        
-        #while len(pmts) != 0:
-        for i in range(len(pmts)):
+        while len(pmts) != 0:
+        #for i in range(len(pmts)):
             startTime = np.inf
             for pmt, pulses in pmts:
-                #print(pulses[0])
                     if pulses.pulses[0].time < startTime:
-                    #print(i)
                         leadTube = pmt
-                #print(leadTube)
+                        #print(type(leadTube))
                     #print("i am here")
                         startTime = pulses.pulses[0].time
             mult = 0
             for pmt, pulses in pmts:
-                #print(pulses)
-                #print(pulses[0])
                 if pulses.pulses[0].time < startTime + timeWindow:
-                    #print(startTime+timeWindow)
-                    #print("boooo")
                     mult = mult+ 1
                 #print(mult)
-                #print(startTime)
+                #print(pmt)
                 if pmt==leadTube:
-                    print(pulses.pulses[0].time)
-                    print(startTime+timeWindow)
-                    print("divide")
+                    #print(pulses.pulses[0].time)
+                    #print(startTime+timeWindow)
+                    #print("divide")
                     assert pulses.pulses[0].time<startTime+timeWindow
                     assert mult>0
             
@@ -126,10 +138,12 @@ def findModuleMultiplicity(modules , timeWindow):
                 trigger.time = startTime
             
             #I am lowkey stuck right here bc idk exactly what this is doing or how to implement in python
-            #pmts[leadTube]
+            print(pmts[leadTube][1])
+            pmts[leadTube][1].advance()
             #this is the part where you erase and instead need to concatenate? 
-            #if len(pmts[leadTube]) == 0:
-                #pmts.remove()
+            if pmts[leadTube][1].empty():
+                pmts.pop(leadTube)
+                print("I have removed")
 
         if trigger.multiplicity:
             triggers.append(trigger)
@@ -159,9 +173,7 @@ def getData():
     for frame in frames:
         pulsemap = frame['PMTResponse_nonoise']
         
-        #create pulsequeue here as a function
-        pulseQueue = []
-        i = 0      
+        #create pulsequeue here as a function   
 
         for omkey, p in pulsemap:
             #if we use a dictionary
